16fd335ebe52f84f7d674a88e37a1c7e229edefd
Dylan Buchi <devybuchi@gmail.com>
2020-10-04 10:03:16 -0300

    fix changing name bug and update main.py

--- main.py
+++ main.py
@@ -30,10 +30,135 @@ DEFAULT_PREFIX = '?'
 client = commands.Bot(command_prefix=DEFAULT_PREFIX, case_insensitive=True)
 
 
+def client_update():
+    # update live the code to get last data
+    servers = client.guilds
+
+    if not servers:
+        print('no server yet')
+        return
+
+    server_ids = {}
+    names = []
+
+    server_id_file = 'clients-server-id.json'
+    folder = 'data'
+    users_file = 'clients-server-name-id.txt'
+
+    if not os.path.exists(users_file):
+        open(f'{get_absolute_file_path(folder, users_file)}', 'w')
+
+    if not os.path.exists(server_id_file):
+        open(f'{get_absolute_file_path(folder, server_id_file)}', 'w')
+
+    for server in servers:
+        name = f'{server.name}-{server.id}'
+        names.append(name)
+        server_ids[server.name] = server.id
+
+    json.dump(
+        server_ids,
+        open(get_absolute_file_path(folder, server_id_file), 'w'),
+        indent=4,
+    )
+
+    with open(f'{get_absolute_file_path(folder, users_file)}', 'w+') as f:
+        for name in names:
+            if name not in f.readlines():
+                f.write(name + '\n')
+        ids = json.load(
+            open(get_absolute_file_path('data', 'clients-server-id.json')))
+
+    # if client.id in ids.values():
+    #     print('id in database')
+    #     filter_id = {'_id': guild.id}
+    #
+    #update data from database if the id exist
+    collections = []
+
+    for id in server_ids.values():
+        filter_id = {'_id': int(id)}
+        guildname = client.get_guild(id).name
+        coll = mongodb.get_database_data(COLLECTION, filter_id)
+        print(coll)
+        print(guildname)
+        print(type(guildname))
+        if coll:
+            print(coll['server name'])
+            if coll['server name'] != guildname and coll:
+                print('true')
+            temp_data = mongodb.get_database_data(COLLECTION, filter_id)
+            print(temp_data)
+            COLLECTION.delete_one(filter_id)
+            temp_data['server name'] = guildname
+            COLLECTION.insert_one(temp_data)
+
+        collections.append(mongodb.get_database_data(COLLECTION, filter_id))
+    for collection in collections:
+        # if not os.path.exists(get_absolute_file_path('data',)) and collection:
+        if collection:
+            server_name, server_id = collection['server name'], collection[
+                '_id']
+            filename = get_server_data_file_name(server_name, server_id)
+            if not os.path.exists(get_absolute_file_path(folder, filename)):
+                open(f'{get_absolute_file_path(folder, filename)}', 'w')
+            else:
+                update_local_server_file(
+                    collection, get_absolute_file_path(folder, filename))
+            json.dump(collection,
+                      open(get_absolute_file_path(
+                          folder,
+                          filename,
+                      ), 'w'),
+                      indent=4)
+
+
+#check local files has the same id if yes delete the older one
+    delete_older_duplicate_file(folder)
+
+
+def delete_older_duplicate_file(folder):
+    stats = {}
+    duplicates = []
+    only_json = list(
+        filter(
+            lambda f: f.endswith('.json') and '-' in f and any(i.isdigit()
+                                                               for i in f),
+            os.listdir(os.path.join(os.getcwd(), folder))))
+
+    for i in range(len(only_json) - 1):
+        item = only_json[i]
+        next_item = only_json[i + 1]
+
+        start = item.find('-') + 1
+        end = item.find('.')
+
+        start_next = next_item.find('-') + 1
+        end_next = next_item.find('.')
+
+        id_ = item[start:end]
+        id_next = next_item[start_next:end_next]
+
+        if id_ == id_next:
+            duplicates.append(item)
+            duplicates.append(next_item)
+
+    print(duplicates)
+
+    for duplicate in duplicates:
+        creation_time = os.stat(
+            os.path.join(get_absolute_file_path(folder, duplicate))).st_ctime
+        stats[duplicate] = creation_time
+
+    print(stats)
+    if (len(stats) > 1):
+        minimum = min(stats.items(), key=lambda x: x[1])
+        os.remove(os.path.join(os.getcwd(), folder, minimum[0]))
+
+
 @client.event
 async def on_guild_update(before, after):
-    #update when server name changes
-
+    client_update()
     old_name = before.name
     new_name = after.name
     guild_id = after.id
@@ -41,6 +166,7 @@ async def on_guild_update(before, after):
     filter_id = {'_id': int(guild_id)}
     temp_data = mongodb.get_database_data(COLLECTION, filter_id)
 
+    # update data when server name changes
     if old_name != new_name:
         try:
             # remove old data once from mongodb database filtered by id
@@ -80,68 +206,71 @@ async def on_guild_update(before, after):
             # create the new file name in github repository
             gh.create_file_in_github_repo(f'data/{new_file_name}', temp_data)
 
+            #update code
+
 
 @client.event
 async def on_ready():
     print('Bot is Ready')
     await client.change_presence(status=discord.Status.online,
                                  activity=discord.Game('Best Bot Game IV'))
-    servers = client.guilds
-    if not servers:
-        print('no server yet')
-        return
+    client_update()
+    # servers = client.guilds
+    # if not servers:
+    #     print('no server yet')
+    #     return
 
-    server_ids = {}
-    names = []
+    # server_ids = {}
+    # names = []
 
-    server_id_file = 'clients-server-id.json'
-    folder = 'data'
-    users_file = 'clients-server-name-id.txt'
+    # server_id_file = 'clients-server-id.json'
+    # folder = 'data'
+    # users_file = 'clients-server-name-id.txt'
 
-    if not os.path.exists(users_file):
-        open(f'{get_absolute_file_path(folder, users_file)}', 'w')
+    # if not os.path.exists(users_file):
+    #     open(f'{get_absolute_file_path(folder, users_file)}', 'w')
 
-    if not os.path.exists(server_id_file):
-        open(f'{get_absolute_file_path(folder, server_id_file)}', 'w')
+    # if not os.path.exists(server_id_file):
+    #     open(f'{get_absolute_file_path(folder, server_id_file)}', 'w')
 
-    for server in servers:
-        name = f'{server.name}-{server.id}'
-        names.append(name)
-        server_ids[server.name] = server.id
+    # for server in servers:
+    #     name = f'{server.name}-{server.id}'
+    #     names.append(name)
+    #     server_ids[server.name] = server.id
 
-    json.dump(
-        server_ids,
-        open(get_absolute_file_path(folder, server_id_file), 'w'),
-        indent=4,
-    )
+    # json.dump(
+    #     server_ids,
+    #     open(get_absolute_file_path(folder, server_id_file), 'w'),
+    #     indent=4,
+    # )
 
-    with open(f'{get_absolute_file_path(folder, users_file)}', 'w+') as f:
-        for name in names:
-            if name not in f.readlines():
-                f.write(name + '\n')
+    # with open(f'{get_absolute_file_path(folder, users_file)}', 'w+') as f:
+    #     for name in names:
+    #         if name not in f.readlines():
+    #             f.write(name + '\n')
 
-    collections = []
+    # collections = []
 
-    for id in server_ids.values():
-        filter_id = {'_id': int(id)}
-        collections.append(mongodb.get_database_data(COLLECTION, filter_id))
-    for collection in collections:
-        # if not os.path.exists(get_absolute_file_path('data',)) and collection:
-        if collection:
-            server_name, server_id = collection['server name'], collection[
-                '_id']
-            filename = get_server_data_file_name(server_name, server_id)
-            if not os.path.exists(get_absolute_file_path(folder, filename)):
-                open(f'{get_absolute_file_path(folder, filename)}', 'w')
-            else:
-                update_local_server_file(
-                    collection, get_absolute_file_path(folder, filename))
-            json.dump(collection,
-                      open(get_absolute_file_path(
-                          folder,
-                          filename,
-                      ), 'w'),
-                      indent=4)
+    # for id in server_ids.values():
+    #     filter_id = {'_id': int(id)}
+    #     collections.append(mongodb.get_database_data(COLLECTION, filter_id))
+    # for collection in collections:
+    #     # if not os.path.exists(get_absolute_file_path('data',)) and collection:
+    #     if collection:
+    #         server_name, server_id = collection['server name'], collection[
+    #             '_id']
+    #         filename = get_server_data_file_name(server_name, server_id)
+    #         if not os.path.exists(get_absolute_file_path(folder, filename)):
+    #             open(f'{get_absolute_file_path(folder, filename)}', 'w')
+    #         else:
+    #             update_local_server_file(
+    #                 collection, get_absolute_file_path(folder, filename))
+    #         json.dump(collection,
+    #                   open(get_absolute_file_path(
+    #                       folder,
+    #                       filename,
+    #                   ), 'w'),
+    #                   indent=4)
 
 
 @client.event
@@ -159,9 +288,9 @@ async def on_member_join(member):
 @client.event
 async def on_guild_join(guild):
     # when the bot join a server (guild)
+
     file_name = botfile.get_server_data_file_name(guild.name, guild.id)
     file_path = botfile.get_absolute_file_path('data', file_name)
-
     if not os.path.exists(file_path):
 
         data = {'_id': int(guild.id), 'server name': guild.name}
@@ -172,20 +301,26 @@ async def on_guild_join(guild):
             sort_keys=True,
             indent=4,
         )
-        if not COLLECTION.find_one({'_id': int(guild.id)}):
-            # insert the data to database and create a file in the github repo
-            COLLECTION.insert_one(data)
-        try:
-            gh.create_file_in_github_repo(f'data/{file_name}', data)
-        except:
-            print('file exists')
+    if not COLLECTION.find_one({'_id': int(guild.id)}):
+        # insert the data to database and create a file in the github repo
+        COLLECTION.insert_one(data)
+
+    try:
+        gh.create_file_in_github_repo(f'data/{file_name}', data)
+
+    except:
+        print('file exists')
+    client_update()
 
 
 @client.event
 async def on_guild_remove(guild):
     # when bot get's removed
     filename = get_server_data_file_name(guild.name, guild.id)
-    os.remove(get_absolute_file_path('data', filename))
+    try:
+        os.remove(get_absolute_file_path('data', filename))
+    except:
+        print("can't delete file")
     print("Bot has been removed")
 
 
@@ -205,7 +340,6 @@ def load_cogs(path, folder):
 
 
 if __name__ == "__main__":
-
     CLIENT, COLLECTION = mongodb.get_database('triggers')
     # for f in os.listdir(os.path.join(os.getcwd(), 'data')):
     #     parts = f.split('-')
