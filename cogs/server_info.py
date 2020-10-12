a1f1bb47a3066ff7770333a7684565d39b2fe33b
Dylan Buchi <devybuchi@gmail.com>
2020-10-11 22:57:15 -0300

    add uptime comand and modify ping

--- cogs/server_info.py
+++ cogs/server_info.py
@@ -1,6 +1,10 @@
-import discord
-import datetime
+import discord, datetime, time
+
 from discord.ext import commands
+from datetime import datetime, timedelta
+
+#bot start time
+start_time = time.time()
 
 
 class Server(commands.Cog):
@@ -10,8 +14,25 @@ class Server(commands.Cog):
     @commands.command(name='ping')
     @commands.has_permissions(manage_guild=True)
     async def ping_command(self, ctx):
+        text = f'🏓 (~{round(self.client.latency, 1)} ms)'
 
-        await ctx.send(f'Pong! (~{round(self.client.latency, 1)} ms)')
+        embed = discord.Embed(colour=discord.Colour.green())
+        embed.add_field(name="Pong!", value=text)
+        await ctx.send(embed=embed)
+
+    @commands.command(name='uptime')
+    @commands.has_permissions(manage_guild=True)
+    async def uptime_command(self, ctx):
+        current_time = time.time()
+        difference = int(round(current_time - start_time))
+        text = str(timedelta(seconds=difference))
+
+        embed = discord.Embed(colour=discord.Colour.red())
+        embed.add_field(name="Uptime", value=text)
+        try:
+            await ctx.send(embed=embed)
+        except discord.HTTPException:
+            await ctx.send("Current uptime: " + text)
 
     # @commands.command(name='info')
     # @commands.has_permissions(manage_guild=True)
